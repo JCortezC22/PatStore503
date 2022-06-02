a7a3c7668924f084e2ae90a83d85a4b004e9b851
JordyC21 <87671558+JordyC21@users.noreply.github.com>
2022-06-02 17:54:03 -0600

    PatStore

---
+++ Base de datos.sql
@@ -0,0 +1,53 @@
+create database PatStore
+
+go
+
+use 
+PatStore
+go
+
+create table Cliente(
+idCliente int primary key identity (1,1)not null,
+Usuario varchar(50) not null,
+Email varchar(50) not null,
+Contraseña varchar (100) not null,
+);
+
+go
+
+create table Producto (
+idProducto int identity(1,1) NOT NULL PRIMARY KEY,
+Nombre varchar(50) not null,
+Descripcion varchar(700) not null,
+Precio decimal(5,2) not null,
+Cantidad int,
+Estado int,
+Foto varchar (1000)
+);
+
+go
+
+
+create table Venta(
+idVenta int primary key identity(1,1) not null,
+idCliente int not null,
+num_comprobante varchar (10) not null,
+fecha_hora datetime default getdate(),
+total decimal (11,2) not null,
+FOREIGN KEY (idCliente) REFERENCES Cliente (idCliente)
+);
+
+go
+
+create table detalle_factura(
+idDetalle_venta integer primary key identity (1,1),
+idVenta int not null,
+idProducto int not null,
+idCliente int not null,
+precio decimal(11,2) not null,
+FOREIGN KEY (idVenta) REFERENCES Venta (idVenta),
+FOREIGN KEY (idProducto) REFERENCES Producto (idProducto),
+FOREIGN KEY (idCliente) REFERENCES Cliente (idCliente)
+);
+
+go
---
+++ PatStore/Coffe_Gourment/Coffe_Gourment/Controllers/IndexPagoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coffe_Gourment.Controllers
+{
+    public class IndexPagoController : Controller
+    {
+        // GET: IndexPago
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
